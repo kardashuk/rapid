76d5601729e89b538422c62fa603d7420045ed0e
dkardashuk <dkardashuk@cogniance.com>
2014-12-23 17:11:01 +0200

    Remove user command

--- commands/UserController.php
+++ commands/UserController.php
@@ -10,6 +10,7 @@ namespace app\commands;
 use yii\console\Controller;
 use yii\mongodb\Collection;
 use Yii;
+use yii\mongodb\Query;
 
 /**
  * This command echoes the first argument that you have entered.
@@ -21,21 +22,31 @@ use Yii;
  */
 class UserController extends Controller
 {
-    /**
-     * This command echoes what you have entered as the message.
-     * @param string $message the message to be echoed.
-     */
     public function actionCreate($username, $password)
     {
+        //check for existence
+        $data = (new Query)->from('users')->where(['username' => $username])->one();
+        if ($data && $data['_id']){
+            echo "Error: User with name $username exists. \nID: ".$data['_id'];
+        }else{
+            //create new user
+            $collection = Yii::$app->mongodb->getCollection('users');
+            $pass = crypt($password, 'rl');
+            $userId = $collection->insert([
+                'username' => $username,
+                'create_date'=>time(),
+                'password'=>$pass
+            ]);
 
+            echo "User created. ID:".$userId;
+        }
+    }
+    public function actionRemove($id){
         $collection = Yii::$app->mongodb->getCollection('users');
-        $pass = crypt($password, 'rl');
-        $userId = $collection->insert([
-            'username' => $username,
-            'create_date'=>time(),
-            'password'=>$pass
-        ]);
-
-        echo "User created. ID:".$userId;
+        if ($collection->remove(['_id' => $id], ["justOne" => true])){
+            echo "User removed";
+        }else{
+            echo "Error: User with id $id doesn`t exist.";
+        }
     }
 }
