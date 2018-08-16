69fed1e2ecb1a5f264eb9b84ccb6b3ad69ec6869
Dmytro Kardashuk <dkardashuk@cogniance.com>
2018-08-16 12:01:39 +0300

    Separate Data & configs

--- index.js
+++ index.js
@@ -1,52 +1,24 @@
 const { ApolloServer, gql } = require('apollo-server');
+const fs = require('fs');
+const orql = require('orql');
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [
-    {
-        title: 'Harry Potter and the Chamber of Secrets',
-        author: 'J.K. Rowling'
-    },
-    {
-        title: 'Jurassic Park',
-        author: 'Michael Crichton'
-    },
-];
+const Data =  JSON.parse(fs.readFileSync('config/data.json').toString());
+var types = fs.readFileSync('config/types.gql').toString();
+const typeDefs = gql(types);
 
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
-const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
-  type Query {
-    books: [Book]
-  }
-`;
-
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
+//API
 const resolvers = {
-        Query: {
-            books: () => books,
-    },
-    };
+    Query: {
+        books: (parent, args, context, info) => {
+            console.log(args.rql, Data.books);
+            console.log(orql(Data.books, args.rql));
+            return orql(Data.books, args.rql);
+        }
+    }
+};
 
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
+//SERVER
 const server = new ApolloServer({ typeDefs, resolvers });
-
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
